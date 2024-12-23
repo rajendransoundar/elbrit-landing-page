75267178ddebe1d7c3f124f9c0573f33f36d0ca8
rajendran.s@engro.io-at-491938176112 <rajendran.s@engro.io>
2024-12-23 10:34:47 +0530

    section width

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ const logoUrl =
   "https://s3-alpha-sig.figma.com/img/8be1/c27b/24873ffb8a3345a8dedea9b0d8f9e7ba?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hscq97n57E-ODprRGY61tc~RF744y6TBqmSUzGgJLQxiOAWXYKSBfEoMgzP5iNnSUG2qPY-8ABEEzsPq5bfwAKS2twDEn0GVSvif8j-WubvROaA21tkcJWyN4dgoBy0PaDOuKcYnhRRtdcpGN5bLju5qGZGRd6rPOiJ7Qge8TlJBqpQ9fBHQbZ8EkTfUa25PdWQf4Bl-V0GqYhFL5GTnsInuzEF8KnoDH2CAb6fa0mAh74VmsZp4dEMW7OdE4FEdCrQU3DmGqF0p-j8c-iUSHep9krfMTEVdLVv536Ta7kpWQE300A9srkPmxeKl-cyzisifRX~XBYB5ZjS~UDZkRg__";
 export default function index() {
   return (
-    <>
+    <nav>
       <Image
         className="header-logo"
         src={logoUrl}
@@ -13,6 +13,6 @@ export default function index() {
         priority
         alt="Elbrit Life Sciences"
       />
-    </>
+    </nav>
   );
 }
